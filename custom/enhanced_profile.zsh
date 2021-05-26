6eab2c2300d934723c11e47625645c3b4d4ad697
Andrew Bengier <andrew.bengier@gmail.com>
2021-05-26 11:35:55 -0400

    [MASTER] - Fix git story function

--- custom/enhanced_profile.zsh
+++ custom/enhanced_profile.zsh
@@ -22,7 +22,7 @@ check_git() {
 }
 
 gcam_story() {
-  git commit -a -m "S{STORY_NUM)$1"
+  git commit -a -m "${STORY_NUM}$1"
 }
 
 change_story() {
@@ -79,25 +79,25 @@ theme() {
 # ------------------------------------- #
 # - Zsh Alias -                         #
 # ------------------------------------- #
-alias zshTips='open -a "Google Chrome" https://devhints.io/zsh'                # zshTips: opens Google Chrome with ZSH tips site
+alias zshTips='open -a "Google Chrome" https://devhints.io/zsh'  # zshTips: opens Google Chrome with ZSH tips site
 
-alias szsh='source $HOME/.zshrc'                                # szsh:    source zshrc
+alias szsh='source $HOME/.zshrc'                                 # szsh:    source zshrc
 
-alias sze='source $HOME/.oh-my-zsh/custom/enhanced_profile.zsh' # sze:     source zsh enhanced profile
-alias eze='edit $HOME/.oh-my-zsh/custom/enhanced_profile.zsh'   # eze:     edits zsh enhanced profile
+alias sze='source $HOME/.oh-my-zsh/custom/enhanced_profile.zsh'  # sze:     source zsh enhanced profile
+alias eze='code -n $HOME/.oh-my-zsh/custom/enhanced_profile.zsh' # eze:     edits zsh enhanced profile
 
-alias quit='exit'                                                              # quit:    works like exit ( closes terminal session )
+alias quit='exit'                                                # quit:    works like exit ( closes terminal session )
 
-alias ll='ls -lahF'                                                            # ll:      ls replacement ( includes l,a,h and F flags )
-alias rm='rm -i'                                                               # rm:      overrides rm ( include i flag )
-alias cp='cp -i'                                                               # rm:      overrides cp ( include i flag )
-alias mv='mv -i'                                                               # rm:      overrides mv ( include i flag )
+alias ll='ls -lahF'                                              # ll:      ls replacement ( includes l,a,h and F flags )
+alias rm='rm -i'                                                 # rm:      overrides rm ( include i flag )
+alias cp='cp -i'                                                 # rm:      overrides cp ( include i flag )
+alias mv='mv -i'                                                 # rm:      overrides mv ( include i flag )
 
-alias cls='clear'                                                              # cls:     windows compatible clear 
+alias cls='clear'                                                # cls:     windows compatible clear 
 
-alias finder='open -a Finder ./'                                               # finder:  opens a file directory in Finder
-alias edit='code'                                                              # edit:    opens a file or directory using VSCode
-alias code='code $PWD'                                                         # code:    opens current direcoty using VSCode
+alias finder='open -a Finder ./'                                 # finder:  opens a file directory in Finder
+alias edit='code'                                                # edit:    opens a file or directory using VSCode
+alias code='code $PWD'                                           # code:    opens current direcoty using VSCode
 
 # ------------------------------------- #
 # - Git -                               #
@@ -111,22 +111,27 @@ alias cstory='change_story'                                      # cstory:  chan
 alias gcam='gcam_story'                                          # gcam:    overrides current gcam with appended story number
 
 alias gro="git config --get remote.origin.url"                   # gro:     checks origin url
+alias gpo="git push origin"                                      # gpo:     pushes to origin remotely
 
 # ------------------------------------- #
 # - Notes -                             #
 # ------------------------------------- #
 export NOTES=$HOME/Documents/Notes
+export logs=$NOTES/daily-notes
 
-alias markdownTips='open -a "Google Chrome" https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet'                                           # markdownTips: opens tips on markdown 
+alias markdownTips='open -a "Google Chrome" https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet'                            # markdownTips:    opens tips on markdown 
 
-alias log='mkdir -p $NOTES/log/$(date +"%Y/%m") && touch $NOTES/log/$(date +"%Y/%m/%Y-%m-%d.md")'                                                       # log:          creates a log entry for today ( markdown ) 
-alias log-o='mkdir -p $NOTES/log/$(date +"%Y/%m") && touch $NOTES/log/$(date +"%Y/%m/%Y-%m-%d.md") && code -n $NOTES/log'                               # log-o:        creates a log entry for today and opens in editor ( markdown )
+alias log='mkdir -p $logs/$(date +"%Y/%m") && touch $logs/$(date +"%Y/%m/%Y-%m-%d.md")'                                                  # log:             creates a log entry for today ( markdown ) 
+alias log-o='mkdir -p $logs/$(date +"%Y/%m") && touch $logs/$(date +"%Y/%m/%Y-%m-%d.md") && code -n $logs'                               # log-o:           creates a log entry for today and opens in editor ( markdown )
 
-alias yesterday='mkdir -p $NOTES/log/$(date -d yesterday +"%Y/%m") && touch $NOTES/log/$(date -d yesterday +"%Y/%m/%Y-%m-%d.md") && code -n $NOTES/log' # yesterday:    creates log entry for yesterday ( same as current log )
+alias yesterday='mkdir -p $logs/$(date -d yesterday +"%Y/%m") && touch $logs/$(date -d yesterday +"%Y/%m/%Y-%m-%d.md") && code -n $logs' # yesterday:       creates log entry for yesterday ( same as current log )
 
-alias project-notes='project_notes'                                                                                                                     # project-notes:   creates new folder for project notes 
-alias project-section='project_section'                                                                                                                 # project-section: creates new folder in provided project ( ex. features, issues )
-alias project-doc='project_doc'                                                                                                                         # project-doc:     creates log ( markdown ) file for specific doc in project section
+alias lsnotes='ll $NOTES'                                                                                                                # lsnotes:         lists all notes folders
+alias lsprojects='ll $NOTES/projects'                                                                                                    # lsprojects:      lists all project notes folders
+
+alias project-notes='project_notes'                                                                                                      # project-notes:   creates new folder for project notes 
+alias project-section='project_section'                                                                                                  # project-section: creates new folder in provided project ( ex. features, issues )
+alias project-doc='project_doc'                                                                                                          # project-doc:     creates log ( markdown ) file for specific doc in project section
 
 # ------------------------------------- #
 # - JDK -                               #
